1202e06442b756c77ad573a4844c7239da6a0b23
Jose <802.275@alunos.unigran.br>
2022-08-24 23:44:39 -0400

    main feita

--- app/src/main/java/com/example/lista_compras/MainActivity.java
+++ app/src/main/java/com/example/lista_compras/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,5 +28,18 @@ public class MainActivity extends AppCompatActivity {
         marca=findViewById(R.id.marcaid);
         quantidade=findViewById(R.id.quantidadeid);
 
+        dados = new ArrayList<>();
+        listagem = findViewById(R.id.listaid);
+        ArrayAdapter adapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,dados);
+        listagem.setAdapter(adapter);
+
+    }
+    public void salvar(View view){
+        lista lista = new lista();
+        lista.setProduto(produto.getText().toString());
+        lista.setMarca(marca.getText().toString());
+        lista.setQuantidade(quantidade.getText().toString());
+        dados.add(lista);
+        Toast.makeText(this,"Item salvo",Toast.LENGTH_SHORT).show();
     }
 }
